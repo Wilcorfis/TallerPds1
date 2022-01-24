59d3a9e609c697fb69f53eed53979bdc21899585
wilcorfis <wilmerandres505@gmail.com>
2022-01-24 18:08:23 -0500

    sds

--- src/main/java/co/com/poli/proyectos/services/ProjectServiceImpl.java
+++ src/main/java/co/com/poli/proyectos/services/ProjectServiceImpl.java
@@ -30,7 +30,17 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public Project create( Project project) {
+
+        Map<String,Object> response = new HashMap<String,Object>();
+        try {
+            response.put("request", project);
+            response.put("status", "success");
+        }catch (Exception e){
+            response.put("status","Your request is invalid");
+        }
         return repository.save(project);
+
+
     }
 
     /*public List<Project> findByIdTasks(Long id)  {
