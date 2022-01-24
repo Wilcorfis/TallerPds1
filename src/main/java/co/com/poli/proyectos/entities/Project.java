413779ac3279dd9560acd4dd921c22c05409cc1a
wilcorfis <wilmerandres505@gmail.com>
2022-01-24 10:47:08 -0500

    poject

--- src/main/java/co/com/poli/proyectos/entities/Project.java
+++ src/main/java/co/com/poli/proyectos/entities/Project.java
@@ -24,8 +24,8 @@ public class Project extends EntityBase{
     @Column(name="project_name",unique=true)
     private String projectName;
 
-    @Size(min = 5)
-    @Size(max= 7)
+    @Size(min = 5,max = 7)
+
     @NotEmpty (message = "Name may not be blank")
     @NotNull  
     @Column(name="project_identifier",unique=true, updatable=false)
