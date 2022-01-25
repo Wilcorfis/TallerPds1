e6c56a33022b43b9948a6f89651ab7b3b59abb4e
wilcorfis <wilmerandres505@gmail.com>
2022-01-25 09:43:04 -0500

    ajustes

--- src/main/java/co/com/poli/proyectos/entities/Project.java
+++ src/main/java/co/com/poli/proyectos/entities/Project.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 
 import javax.persistence.*;
-
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
