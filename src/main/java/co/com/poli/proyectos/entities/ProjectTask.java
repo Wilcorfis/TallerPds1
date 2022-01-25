cb9bf79f6745ead1cb654c03c65e8492433051d1
wilcorfis <wilmerandres505@gmail.com>
2022-01-25 09:21:29 -0500

    noblank

--- src/main/java/co/com/poli/proyectos/entities/ProjectTask.java
+++ src/main/java/co/com/poli/proyectos/entities/ProjectTask.java
@@ -21,12 +21,12 @@ public class ProjectTask extends EntityBase {
 
     @Column(name="name")
     @NotEmpty
-    //@NotBlank
+    @NotBlank
     private String name;
 
     @Column(name="summary")
     @NotEmpty
-    //@NotBlank
+    @NotBlank
     private String summary;
 
     @Column(name="acceptance_criteria")
@@ -62,7 +62,8 @@ public class ProjectTask extends EntityBase {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="backlog_id")
     @NotEmpty
-    //@NotBlank
+    @NotBlank
+    @NotNull
     private Backlog backlog;
 
     @Override
